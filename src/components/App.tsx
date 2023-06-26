c38d76fae525c64420df88e910d031bdaef4f59d
khanchenkov <daniilkhanchenkov@gmail.com>
2023-06-26 13:20:16 +0300

    project init

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import { observer } from 'mobx-react'
+import { useEffect } from 'react'
+import Logo from '../logo.svg'
+
+const App = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+}
+
+export default App
